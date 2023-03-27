baf5f6f08c4980dad20da23ddba3029ff3186199
Ravi Kumar <ravi14577@gmail.com>
2023-03-27 22:16:11 +0530

    Final edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import Form from "./Form"
 import TodoList from "./TodosList"
 const App = () =>{
 
+  //store our todolist items in our local storage
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
    //useState hooks for keep traacking what the user input int the todo
    const [input,setInput] =useState("");
   //useState hooks for keep traacking what all the todo in the list
-   const [todos,setTodos] =useState([]);
+   const [todos,setTodos] =useState(initialState);
+
+   const[editTodo , setEditTodo] = useState(null);
+
+   useEffect(() =>{
+    localStorage.setItem("todos" , JSON.stringify(todos));
+   }, [todos]);
   return(
 
-    <div className="main_div">
-      <div className="center_div">
+    <div className="container">
+      <div className="app-wrapper">
         <div>
         <h1> ToDo List</h1>
         </div>
@@ -22,11 +30,13 @@ const App = () =>{
           setInput={setInput}
           todos={todos}
           setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
           />
         </div>
 
         <div>
-          <TodoList  todos={todos} setTodos={setTodos} />
+          <TodoList  todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
         </div>
       </div>
     </div>
